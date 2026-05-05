0ca2d36dc4a767da3bec76696d87b0e2255b463c
Mazid <mazidyerima2@gmail.com>
2026-05-05 19:01:21 +0000

    I just created the board. I am able to store a player's choice on the board, so that another input can be taken.

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print('Start here')
+dict = {1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}
+def get_board(spot, player, dict):  
+
+    for key in dict:
+        if key == spot:
+            dict[key] = player
+  
+    print(f" {dict[1]} | {dict[2]} | {dict[3]}")
+    print("---+---+---")
+    print(f" {dict[4]} | {dict[5]} | {dict[6]}")
+    print("---+---+---")
+    print(f" {dict[7]} | {dict[8]} | {dict[9]}")
+
+    return dict
+
+dyc = get_board(5, 'X', dict)
+get_board(4, "O", dyc)
+
+#i am able to print a board and also place my players in the squares. A board is reusable, permitting the game to continue.
+ 
+
+
