c2a82f2ceb8e73fed9ffebc0772e47acae28b597
Mazid <mazidyerima2@gmail.com>
2026-05-06 12:24:46 +0000

    I created another function to check the winning cases. also i remove the player choice's input from the 'place_piece()' function to put it in argument. I changed obviously the name of the function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 dict = {1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}
+
 def get_board(dict):  
     print(f" {dict[1]} | {dict[2]} | {dict[3]}")
     print("---+---+---")
@@ -6,17 +7,25 @@ def get_board(dict):
     print("---+---+---")
     print(f" {dict[7]} | {dict[8]} | {dict[9]}")
 
-def play_aturn(player, dictionary):
-    player_choice = input('Enter your choice: ')
+def place_piece(player, dictionary, choice):
     for key in dictionary:
-        if key == int(player_choice):
+        if key == int(choice):
             dictionary[key] = player
     new_dictionary = dictionary
 
     return new_dictionary
 
-dyc = play_aturn('O', dict)
-get_board(dyc)
+def check_win(dict):
+    if dict[1] == dict[2] == dict[3] != " " or dict[1] == dict[4] == dict[7] != " " or dict[1] == dict[5] == dict[9]!= " " or dict[3] == dict[5] == dict[9] != " " or dict[7] == dict[8] == dict[9] != " " or dict[3] == dict[5] == dict[7] != " ":
+        return True
+    
+
+
+
+
+
+        
+
 
 
 
