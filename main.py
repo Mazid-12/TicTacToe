83589da6ef52c590654ed22ed1718b8ecbca5f5d
Mazid <mazidyerima2@gmail.com>
2026-05-06 12:30:19 +0000

    I added a function check_turn() that manages a round

--- main.py
+++ main.py
@@ -18,15 +18,17 @@ def place_piece(player, dictionary, choice):
 def check_win(dict):
     if dict[1] == dict[2] == dict[3] != " " or dict[1] == dict[4] == dict[7] != " " or dict[1] == dict[5] == dict[9]!= " " or dict[3] == dict[5] == dict[9] != " " or dict[7] == dict[8] == dict[9] != " " or dict[3] == dict[5] == dict[7] != " ":
         return True
+def check_turns(dict):
+
+    for player in ['X', 'O']:
+        player_choice = input(f'Player {player} Enter your choice: ')
+        new_dict = place_piece(player, dict, player_choice)
+        get_board(new_dict)
+        dict = new_dict
     
+    return dict
 
-
-
-
-
-        
-
-
+dyc = check_turns(dict)
 
 
     
