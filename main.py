fd57ec4ea52c8c659cc258746be75f291dc50ffa
Mazid <mazidyerima2@gmail.com>
2026-05-06 08:01:32 +0000

    Now, I created a function that gets a player input. The function returns the new dictionary that will help us print the board

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 dict = {1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}
-def get_board(spot, player, dict):  
-
-    for key in dict:
-        if key == spot:
-            dict[key] = player
-  
+def get_board(dict):  
     print(f" {dict[1]} | {dict[2]} | {dict[3]}")
     print("---+---+---")
     print(f" {dict[4]} | {dict[5]} | {dict[6]}")
     print("---+---+---")
     print(f" {dict[7]} | {dict[8]} | {dict[9]}")
 
-    return dict
+def play_aturn(player, dictionary):
+    player_choice = input('Enter your choice: ')
+    for key in dictionary:
+        if key == int(player_choice):
+            dictionary[key] = player
+    new_dictionary = dictionary
 
-dyc = get_board(5, 'X', dict)
-get_board(4, "O", dyc)
+    return new_dictionary
 
-#i am able to print a board and also place my players in the squares. A board is reusable, permitting the game to continue.
- 
+dyc = play_aturn('O', dict)
+get_board(dyc)
 
 
+
+    
