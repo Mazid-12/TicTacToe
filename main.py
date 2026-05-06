7b4aafa0cc7fcded18327a500880aa86eb8e947c
Mazid <mazidyerima2@gmail.com>
2026-05-06 16:54:36 +0000

    I realized that the function check_win() doesn't work, certainly because I didn't specify the 'else' case. so when i did it it started working. I also updated the function continue_play() which is the function that manages multiple rounds.

--- main.py
+++ main.py
@@ -16,19 +16,40 @@ def place_piece(player, dictionary, choice):
     return new_dictionary
 
 def check_win(dict):
-    if dict[1] == dict[2] == dict[3] != " " or dict[1] == dict[4] == dict[7] != " " or dict[1] == dict[5] == dict[9]!= " " or dict[3] == dict[5] == dict[9] != " " or dict[7] == dict[8] == dict[9] != " " or dict[3] == dict[5] == dict[7] != " ":
+    if dict[1] == dict[2] == dict[3] != ' ' or dict[1] == dict[4] == dict[7] != ' ' or dict[1] == dict[5] == dict[9]!= ' ' or dict[3] == dict[5] == dict[9] != ' ' or dict[7] == dict[8] == dict[9] != ' ' or dict[3] == dict[5] == dict[7] != ' ':
         return True
-def check_turns(dict):
-
-    for player in ['X', 'O']:
-        player_choice = input(f'Player {player} Enter your choice: ')
-        new_dict = place_piece(player, dict, player_choice)
-        get_board(new_dict)
-        dict = new_dict
+    else:
+        return False
+    
+def play_round(dict, player):
+    #for player in ['X', 'O']:
+    player_choice = input(f'Player {player} Enter your choice: ')
+    new_dict = place_piece(player, dict, player_choice)
+    get_board(new_dict)
+    dict = new_dict
     
     return dict
 
-dyc = check_turns(dict)
 
+def continue_play(dict):
+    #print(type(dict[1]))
+    win = check_win(dict)
+    #print(win)
+    while win == False:
+        for player in ['X', 'O']:
+            new_dict = play_round(dict, player)
+            print(new_dict)
+        win = check_win(new_dict)
+        #print(win)
 
+    else:
+        print('You win!')
+    
+continue_play(dict)
+        
+'''def pos(val1, val2):
+    if val1 == val2 != ' ' or val1 == 4 != ' ':
+        return True
+res = pos(4, 4)
+print(res)'''
     
