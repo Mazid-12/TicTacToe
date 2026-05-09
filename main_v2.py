7ca1b0a0efb0ef67d48e929779a6a7c38028c279
Mazid <mazidyerima2@gmail.com>
2026-05-09 19:56:59 +0000

    i just create a new function that validates the player's input. It needs to be an integer between 1 and 9

--- main_v2.py
+++ main_v2.py
@@ -1,1 +1,22 @@
-print('New version')
+def print_board(dict):  
+    print(f" {dict[1]} | {dict[2]} | {dict[3]}")
+    print("---+---+---")
+    print(f" {dict[4]} | {dict[5]} | {dict[6]}")
+    print("---+---+---")
+    print(f" {dict[7]} | {dict[8]} | {dict[9]}")
+
+def check_win(dict):
+    if dict[1] == dict[2] == dict[3] != ' ' or dict[1] == dict[4] == dict[7] != ' ' or dict[1] == dict[5] == dict[9]!= ' ' or dict[3] == dict[5] == dict[7] != ' ' or dict[7] == dict[8] == dict[9] != ' ' or dict[3] == dict[6] == dict[9] != ' ' or dict[4] == dict[5] == dict[6] != ' ' or dict[2] == dict[5] == dict[8] != ' ':
+        return True
+    else:
+        return False
+    
+def validate_input(dict, player_input):
+    try:
+        input = int(player_input)
+        if input <=9 and input>= 1:
+            return True
+        else: 
+            return False
+    except ValueError:
+        return False
