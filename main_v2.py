bd6adb094661afd676fbc24d3c4f233b9cdada63
Mazid <mazidyerima2@gmail.com>
2026-05-10 09:55:26 +0000

    Version 2 completed. I refactored the code, I learnt that the main() function needs to be the game controller. Functions should only handle a specific task, not the game in itself. the game controller should have the game flow. So I reduced the functions I have. I have 3 functions: print_board(), check_win(), validate_input(). That's all. main() does everything else.

--- main_v2.py
+++ main_v2.py
@@ -11,7 +11,8 @@ def check_win(dict):
     else:
         return False
     
-def validate_input(dict, player_input):
+def validate_input(player_input):
+
     try:
         input = int(player_input)
         if input <=9 and input>= 1:
@@ -20,25 +21,58 @@ def validate_input(dict, player_input):
             return False
     except ValueError:
         return False
+
 def main():
-    board_dictionary = {1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}
-    input("Press ENTER to start the game.")
+        board_dictionary = {1:' ', 2:' ', 3:' ', 4:' ', 5:' ', 6:' ', 7:' ', 8:' ', 9:' '}
+        print_board(board_dictionary)
+        for round in range(3):
+            for player in ['X', 'O']:
+                while True:
+                    player_choice = input(f'Player {player} Enter your choice (1-9): ')
+                    is_valid = validate_input(player_choice)
+                    if not is_valid:
+                        print('The input is invalid!')
+                        continue
+                    if board_dictionary[int(player_choice)] == ' ':
+                        board_dictionary[int(player_choice)] = player
+                        print_board(board_dictionary)
+                        has_won = check_win(board_dictionary)
+                        if has_won:
+                            return f'Game Over! Player {player} won!'
+                        break
+                    else:
+                        print('This spot is occupied!')                   
+        else:          
+            for round in range(3):
+                for player in ['X', 'O']:
+                    while True:
+                        old_spot = int(input(f'Player {player} Where is the marker you want to move: '))
+                        new_spot = int(input(f'Player {player} Where do you want to move it to: '))
+                        old_isvalid = validate_input(old_spot)
+                        new_isvalid = validate_input(new_spot)
+                        if not old_isvalid and not new_isvalid:
+                            print('The input is invalid!')
+                            continue
+                        if board_dictionary[old_spot] != player:
+                            print('The old spot is occupied by your opponent or empty')
+                            continue
+                        if board_dictionary[new_spot] == ' ':
+                            board_dictionary[old_spot] = ' '
+                            board_dictionary[new_spot] = player
+                            print_board(board_dictionary)
+                            has_won = check_win(board_dictionary)
+                            if has_won:
+                                return f'Game Over! Player {player} won!'        
+                            break      
+                        else:
+                            print('The new spot is not empty')
+            else:
+                return f"Game Over! It's a draw"
 
-    for round in range(3):
-        for player in ['O', 'X']:
-            while True:
-                player_choice = int(input(f'Player {player} Enter your choice (1-9): '))
-                is_valid = validate_input(player_choice)
-                if not is_valid:
-                    ('The input is invalid!')
-                if board_dictionary[player_choice] == ' ':
-                    board_dictionary[player_choice] = player
-                    print_board(board_dictionary)
-                    has_won = check_win(board_dictionary)
-                    if has_won:
-                        return f'Game Over! Player {player} won!'
-        else:
-            return "It's a draw"
-    
-                    
-
+while True:
+    input("Press ENTER to start the game==")
+    result = main()
+    print(result)
+    restart = input('Do you want to play again (0 to leave): ')
+    if restart == '0':
+        break
